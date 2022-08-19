0d468f3ff65c82bc6888af0504c1e77e49b0151b
Reza Rahman <81400350+rahmanreza646@users.noreply.github.com>
2022-08-19 23:43:47 +0600

    Create 228A.cpp

---
+++ 228A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set<int> s;
+
+  for(int i=0;i<4;i++)
+    {
+      int n;
+      cin>>n;
+      s.insert(n);
+      
+    }
+  cout<<4-s.size()<<endl;
+ return 0;
+}
