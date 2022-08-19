5f15359c8a5acaf242c62b5bef2464da9fc510e8
Reza Rahman <81400350+rahmanreza646@users.noreply.github.com>
2022-08-19 19:13:46 +0600

    Create 208A.cpp

---
+++ 208A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+  string str;
+  cin>>str;
+ 
+  for(int i=0;i<str.size();i++)
+    {
+      if(str[i]=='W' && str[i+1]=='U' && str[i+2]=='B')
+      {
+        cout<<" ";
+        i+=2;
+      }
+
+      else
+        cout<<str[i];
+    }
+
+  
+  cout<<endl;
+    return 0;
+}
