b26a9552ce9fba1d84d38ed76c9439c4a0b23d7e
Reza Rahman <81400350+rahmanreza646@users.noreply.github.com>
2022-08-22 20:27:49 +0600

    Create 1335A.cpp

---
+++ 1335A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+    {
+  long long int n;
+  cin>>n;
+
+  if(n==1 || n==2)
+  {
+    cout<<"0"<<endl;
+  }
+  else
+  cout<<(n-1)/2<<endl;
+
+      }
+  return 0;
+  }
